089b037a6c4fdfa3a4032313b78fa44a96aa8ce4
Prasanna Dubey <prasanna.dubey@wisestep-inc.com>
2022-09-10 12:45:50 +0530

    Removing unnecssary imports

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,6 +1,3 @@
-import 'dart:io';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/widgets/adpative_button.dart';
 import 'package:intl/intl.dart';
